16bdb389d9ed0b5dee048227ee137efe6bca8910
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2020-02-25 12:41:39 -0600

    Changed sort method to take only ascending/descending parameter.
    Added code for descending mode.

--- Project 2 CS2336/src/LinkedList.java
+++ Project 2 CS2336/src/LinkedList.java
@@ -53,48 +53,93 @@ public class LinkedList<T extends Comparable<T>>
 		return size;
 	}
 	
-	public void sort(boolean byName, boolean byAscending)
+	//Used to be: 	public void sort(boolean byName, boolean byAscending) but compare should happen based on payload's flag
+	public void sort(boolean byAscending)
 	{
 		Node<T> firstNode = listHead;
 		Node<T> currentNode = firstNode.getNext();
-		Node<T> smallestNode = firstNode;
+		Node<T> smallestOrLargestNode = firstNode; // Holds the smallest/largest node when sorting by ascending/descending
 		
-		while(firstNode != listTail)
+		if(byAscending == true)
 		{
-		    while(currentNode != null)
-		    {
-		    	if(smallestNode.compareTo(currentNode) >= 1)
-		    	{
-		    		smallestNode = currentNode;
-		    	}
-
-		    	if(smallestNode == listTail)
-		    	{
-		    		listTail = firstNode;
-		    	} //since firstNode and smallestNode will be switched, set the new tail
-
-			   currentNode = currentNode.getNext();
-		    }
-		    
-		    //switch nodes pointers
-		    Node<T> tempNext = firstNode.getNext();
-		    Node<T> tempPrevious = firstNode.getPrevious();
-		    
-		    firstNode.setNext(smallestNode.getNext());
-		    firstNode.setPrevious(smallestNode.getPrevious());
-
-		    smallestNode.setNext(tempNext);
-		    smallestNode.setPrevious(tempPrevious);
-
-		    firstNode = smallestNode.getNext();
-		    
-
-		    if(firstNode != listTail)
-		    {
-		       currentNode = firstNode.getNext();
-		    }
-		}
-	}
+			while(firstNode != listTail)
+			{
+			    while(currentNode != null)
+			    {
+			    	if(smallestOrLargestNode.compareTo(currentNode) >= 1)
+			    	{
+			    		smallestOrLargestNode = currentNode;
+			    	}
+	
+			    	if(smallestOrLargestNode == listTail)
+			    	{
+			    		listTail = firstNode;
+			    	} //since firstNode and smallestNode will be switched, set the new tail
+	
+				   currentNode = currentNode.getNext();
+			    }
+			    
+			    //switch nodes pointers
+			    Node<T> tempNext = firstNode.getNext();
+			    Node<T> tempPrevious = firstNode.getPrevious();
+			    
+			    firstNode.setNext(smallestOrLargestNode.getNext());
+			    firstNode.setPrevious(smallestOrLargestNode.getPrevious());
+	
+			    smallestOrLargestNode.setNext(tempNext);
+			    smallestOrLargestNode.setPrevious(tempPrevious);
+	
+			    firstNode = smallestOrLargestNode.getNext();
+			    
+	
+			    if(firstNode != listTail)
+			    {
+			       currentNode = firstNode.getNext();
+			    }
+			}
+			
+		} //if byAscending true end
+		
+		if(byAscending == false)
+		{
+			while(firstNode != listTail)
+			{
+			    while(currentNode != null)
+			    {
+			    	if(smallestOrLargestNode.compareTo(currentNode) <= -1)
+			    	{
+			    		smallestOrLargestNode = currentNode;
+			    	}
+	
+			    	if(smallestOrLargestNode == listTail)
+			    	{
+			    		listTail = firstNode;
+			    	} //since firstNode and smallestNode will be switched, set the new tail
+	
+				   currentNode = currentNode.getNext();
+			    }
+			    
+			    //switch nodes pointers
+			    Node<T> tempNext = firstNode.getNext();
+			    Node<T> tempPrevious = firstNode.getPrevious();
+			    
+			    firstNode.setNext(smallestOrLargestNode.getNext());
+			    firstNode.setPrevious(smallestOrLargestNode.getPrevious());
+	
+			    smallestOrLargestNode.setNext(tempNext);
+			    smallestOrLargestNode.setPrevious(tempPrevious);
+	
+			    firstNode = smallestOrLargestNode.getNext();
+			    
+	
+			    if(firstNode != listTail)
+			    {
+			       currentNode = firstNode.getNext();
+			    }
+			}
+			
+		} //if byAscending false end
+	} // sort function end
 	
 	@Override
 	public String toString()
